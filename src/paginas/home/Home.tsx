6352bc7d6301a0118eb3ad2ff22b04a60ec76815
JhonnFTBR <jhonnatan_evil@hotmail.com>
2022-04-26 23:55:04 -0300

    usando React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react' ;
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+             <img src="https://1.bp.blogspot.com/-qrE83QnqFr0/TWR6PqdiBtI/AAAAAAAALTI/8-oAwSUyPqE/s1600/Chevrolet-Omega-2006-V6-frente.JPG" alt="Imagem Tela Inicial"  className='img'/>
+        </>
+    );
+}
+
+export default Home;
